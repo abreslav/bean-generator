a99eb7b5ad51a597b5422deb312299784a72e07d
Andrey Breslav <andrey.breslav@jetbrains.com>
2012-08-14 11:45:41 +0400

    Mutable bean implementations generated re-using data from interfaces

--- src/org/jetbrains/jet/lang/descriptors/builders/generator/MutableBeanClassesGenerator.java
+++ src/org/jetbrains/jet/lang/descriptors/builders/generator/MutableBeanClassesGenerator.java
@@ -16,203 +16,234 @@
 
 package org.jetbrains.jet.lang.descriptors.builders.generator;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.jet.lang.descriptors.builders.generator.java.ClassPrinter;
 import org.jetbrains.jet.lang.descriptors.builders.generator.java.code.CodeFactory;
 import org.jetbrains.jet.lang.descriptors.builders.generator.java.code.CodeUtil;
 import org.jetbrains.jet.lang.descriptors.builders.generator.java.code.PieceOfCode;
-import org.jetbrains.jet.lang.descriptors.builders.generator.java.declarations.FieldModel;
-import org.jetbrains.jet.lang.descriptors.builders.generator.java.declarations.MethodModel;
-import org.jetbrains.jet.lang.descriptors.builders.generator.java.declarations.Visibility;
+import org.jetbrains.jet.lang.descriptors.builders.generator.java.declarations.*;
 import org.jetbrains.jet.lang.descriptors.builders.generator.java.declarations.beans.ClassBean;
 import org.jetbrains.jet.lang.descriptors.builders.generator.java.declarations.beans.FieldBean;
+import org.jetbrains.jet.lang.descriptors.builders.generator.java.declarations.beans.JavaDeclarationUtil;
 import org.jetbrains.jet.lang.descriptors.builders.generator.java.declarations.beans.MethodBean;
-import org.jetbrains.jet.lang.descriptors.builders.generator.java.declarations.beans.ParameterBean;
 
+import java.util.Collection;
 import java.util.Map;
+import java.util.Queue;
+
+import static org.jetbrains.jet.lang.descriptors.builders.generator.MutableBeanGenerator.*;
 
 /**
 * @author abreslav
 */
 public class MutableBeanClassesGenerator extends EntityRepresentationGenerator {
 
+    private final EntityRepresentationContext<ClassBean> mutableBeanInterfaces;
+
+    public MutableBeanClassesGenerator(EntityRepresentationContext<ClassBean> mutableBeanInterfaces) {
+        this.mutableBeanInterfaces = mutableBeanInterfaces;
+    }
+
+    @NotNull
     @Override
-    protected void generateSupertypes(EntityRepresentationContext<ClassBean> context, ClassBean classBean, Entity entity) {
-
-    }
-
-    @Override
-    protected void generateClassMembers(EntityRepresentationContext<ClassBean> context, ClassBean bean, Entity entity) {
-        Context c = new Context(context, entity, bean);
-        createFields(c);
-        createGetters(c);
-        createSettersAndAdders(c);
-    }
-
-    private void createSettersAndAdders(Context context) {
-        for (final Relation<?> relation : context.entity.getRelations()) {
-            if (!relation.getMultiplicity().isCollection()) {
-                context.classBean.getMethods().add(createSetter(context, relation));
-            }
-            else {
-                context.classBean.getMethods().add(createSingleElementAdder(context, relation));
-                context.classBean.getMethods().add(createAllElementAdder(context, relation));
-            }
-        }
-    }
-
-    private MethodModel createAllElementAdder(final Context context, final Relation<?> relation) {
-        assert relation.getMultiplicity().isCollection();
-
-        return createSelfReturningMethod(context.classBean)
-                .setName(getAllElementAdderName(relation))
-                .addParameter(createAllAdderParameter(context.types, relation))
-                .put(
-                        ClassPrinter.METHOD_BODY,
-                        new PieceOfCode() {
-                            @NotNull
-                            @Override
-                            public <E> E create(@NotNull CodeFactory<E> f) {
-                                return CodeUtil.block(f,
-                                                      CodeUtil.methodCallStatement(f,
-                                                                                   f.fieldReference(f._this(),
-                                                                                                    context.fields.get(relation).getName()),
-                                                                                   "add",
-                                                                                   f.variableReference("value")),
-                                                      f._return(f._this())
-                                );
-                            }
-                        }
-                );
-    }
-
-    private MethodModel createSingleElementAdder(final Context context, final Relation<?> relation) {
-        assert relation.getMultiplicity().isCollection();
-
-        return createSelfReturningMethod(context.classBean)
-                .setName(getSingleElementAdderName(relation))
-                .addParameter(createSetterParameter(context.types, relation))
-                .put(
-                        ClassPrinter.METHOD_BODY,
-                        new PieceOfCode() {
-                            @NotNull
-                            @Override
-                            public <E> E create(@NotNull CodeFactory<E> f) {
-                                return CodeUtil.block(f,
-                                                      CodeUtil.methodCallStatement(f,
-                                                                                   f.fieldReference(f._this(),
-                                                                                                    context.fields.get(relation).getName()),
-                                                                                   "add",
-                                                                                   f.variableReference("value")),
-                                                      f._return(f._this())
-                                );
-                            }
-                        }
-                );
-    }
-
-    private MethodBean createSetter(final Context context, final Relation<?> relation) {
-        return createSelfReturningMethod(context.classBean)
-                .setName(getSetterName(relation))
-                .addParameter(createSetterParameter(context.types, relation))
-                .put(
-                        ClassPrinter.METHOD_BODY,
-                        new PieceOfCode() {
-                            @NotNull
-                            @Override
-                            public <E> E create(@NotNull CodeFactory<E> f) {
-                                return CodeUtil.block(f,
-                                                      f.assignment(
-                                                              f.fieldReference(f._this(), context.getField(relation).getName()),
-                                                              f.variableReference("value")),
-                                                      f._return(f._this())
-                                );
-                            }
-                        }
-                );
-    }
-
-    private ParameterBean createSetterParameter(TypeTransformer types, Relation<?> relation) {
-        return new ParameterBean().addAnnotation(NOT_NULL).setType(types.targetToType(relation.getTarget(), Multiplicity.ONE)).setName("values");
-    }
-
-    private ParameterBean createAllAdderParameter(TypeTransformer types, Relation<?> relation) {
-        assert relation.getMultiplicity().isCollection();
-        return new ParameterBean().addAnnotation(NOT_NULL).setType(
-                types.targetToType(relation.getTarget(), Multiplicity.COLLECTION, TypeTransformer.Variance.OUT)).setName("value");
-    }
-
-    private static String getAllElementAdderName(Relation<?> relation) {
-        return "addAllTo" + relation.getName();
-    }
-
-    private static String getSingleElementAdderName(Relation<?> relation) {
-        return "addTo" + relation.getName();
-    }
-
-    private static MethodBean createSelfReturningMethod(ClassBean classBean) {
-        return new MethodBean()
-                        .addAnnotation(NOT_NULL)
-                        .setVisibility(Visibility.PUBLIC)
-                        .setReturnType(simpleType(classBean));
-    }
-
-    private void createGetters(final Context context) {
-        for (final Relation<?> relation : context.entity.getRelations()) {
-            context.classBean.getMethods().add(new MethodBean()
-                                          .addAnnotation(OVERRIDE)
-                                          .setVisibility(Visibility.PUBLIC)
-                                          .setReturnType(context.types.relationToType(relation))
-                                          .setName(getGetterName(relation))
-                                          .put(
-                                              ClassPrinter.METHOD_BODY,
-                                              new PieceOfCode() {
-                                                  @NotNull
-                                                  @Override
-                                                  public <E> E create(@NotNull CodeFactory<E> f) {
-                                                      return f._return(
-                                                          f.fieldReference(f._this(), context.getField(relation).getName())
-                                                      );
-                                                  }
-                                              }
-                                          )
-            );
-        }
-    }
-
-    private void createFields(Context context) {
-        for (Relation<?> relation : context.entity.getRelations()) {
-            FieldBean field = new FieldBean()
-                    .setVisibility(Visibility.PRIVATE)
-                    .setType(context.types.relationToType(relation))
-                    .setName(getFieldName(relation));
-            context.fields.put(relation, field);
-            context.classBean.getFields().add(field);
-        }
+    protected ClassKind getClassKind() {
+        return ClassKind.CLASS;
     }
 
     @Override
     public String getEntityRepresentationName(@NotNull Entity entity) {
-        return entity.getName() + "Bean";
+        return entity.getName() + "BeanImpl";
     }
 
-    private static class Context {
+    @Override
+    protected void generateSupertypes(EntityRepresentationContext<ClassBean> context, ClassBean classBean, Entity entity) {
+        classBean.getSuperInterfaces().add(simpleType(mutableBeanInterfaces.getRepresentation(entity)));
+    }
+
+    @Override
+    protected void generateClassMembers(EntityRepresentationContext<ClassBean> context, ClassBean classBean, Entity entity) {
+        ClassBean interfaceBean = mutableBeanInterfaces.getRepresentation(entity);
+        EntityContext c = new EntityContext(mutableBeanInterfaces, entity, classBean);
+        createFields(c);
+        Map<String, MethodModel> methodsToImplement = Maps.newHashMap();
+        collectAllMethodsToImplement(methodsToImplement, entity, mutableBeanInterfaces, IMPLS.keySet());
+        for (MethodModel method : methodsToImplement.values()) {
+            for (Map.Entry<DataHolderKey<Relation<?>>, MethodImplementation> entry : IMPLS.entrySet()) {
+                Relation<?> relation = method.getData(entry.getKey());
+                if (relation != null) {
+                    if (c.fields.get(relation) == null) {
+                        createField(c, relation);
+                    }
+                    MethodBean implementation = implement(method, entry.getValue().createBody(c, method));
+                    if (entry.getKey() != GETTER) {
+                        implementation.setReturnType(simpleType(interfaceBean));
+                    }
+                    classBean.getMethods().add(implementation);
+                }
+            }
+        }
+
+    }
+
+    private static void collectAllMethodsToImplement(
+            Map<String, MethodModel> result,
+            Entity start,
+            EntityRepresentationContext<ClassBean> context,
+            Collection<DataHolderKey<Relation<?>>> keys
+    ) {
+        Queue<Entity> queue = Lists.newLinkedList();
+        queue.offer(start);
+        while (!queue.isEmpty()) {
+            Entity entity = queue.remove();
+            ClassBean classBean = context.getRepresentation(entity);
+            for (MethodModel method : classBean.getMethods()) {
+                for (DataHolderKey<Relation<?>> key : keys) {
+                    Relation<?> relation = method.getData(key);
+                    String name = method.getName();
+                    if (relation != null && !result.containsKey(name)) {
+                        result.put(name, method);
+                    }
+                }
+            }
+            for (Entity superEntity : entity.getSuperEntities()) {
+                queue.offer(superEntity);
+            }
+        }
+    }
+
+    private static void createFields(EntityContext context) {
+        for (Relation<?> relation : context.entity.getRelations()) {
+            createField(context, relation);
+        }
+    }
+
+    private static void createField(EntityContext context, Relation<?> relation) {
+        FieldBean field = new FieldBean()
+                .setVisibility(Visibility.PRIVATE)
+                .setType(context.types.relationToType(relation))
+                .setName(getFieldName(relation));
+        context.fields.put(relation, field);
+        context.classBean.getFields().add(field);
+    }
+
+    private static MethodBean implement(MethodModel method, PieceOfCode body) {
+        return JavaDeclarationUtil.copy(method)
+                .addAnnotation(OVERRIDE)
+                .setAbstract(false)
+                .put(ClassPrinter.METHOD_BODY, body);
+    }
+
+    private interface MethodImplementation {
+        PieceOfCode createBody(EntityContext context, MethodModel method);
+    }
+
+    private static MethodImplementation GETTER_IMPL = new MethodImplementation() {
+        @Override
+        public PieceOfCode createBody(final EntityContext context, final MethodModel method) {
+            return new PieceOfCode() {
+               @NotNull
+               @Override
+               public <E> E create(@NotNull CodeFactory<E> f) {
+                   Relation<?> relation = method.getData(GETTER);
+                   return f._return(
+                           f.fieldReference(f._this(), context.getField(relation).getName())
+                   );
+               }
+            };
+        }
+    };
+
+    private static MethodImplementation SETTER_IMPL = new MethodImplementation() {
+        @Override
+        public PieceOfCode createBody(final EntityContext context, final MethodModel method) {
+            return new PieceOfCode() {
+                @NotNull
+                @Override
+                public <E> E create(@NotNull CodeFactory<E> f) {
+                    Relation<?> relation = method.getData(SETTER);
+                    return CodeUtil.block(f,
+                                          f.assignment(
+                                                  f.fieldReference(f._this(), context.getField(relation).getName()),
+                                                  f.variableReference("value")),
+                                          f._return(f._this())
+                    );
+                }
+            };
+        }
+    };
+
+    private static MethodImplementation ADDER_IMPL = new MethodImplementation() {
+        @Override
+        public PieceOfCode createBody(final EntityContext context, final MethodModel method) {
+            return new PieceOfCode() {
+                @NotNull
+                @Override
+                public <E> E create(@NotNull CodeFactory<E> f) {
+                    Relation<?> relation = method.getData(ADDER);
+                    return CodeUtil.block(f,
+                                          CodeUtil.methodCallStatement(f,
+                                                                       f.fieldReference(f._this(),
+                                                                                        context.fields.get(relation).getName()),
+                                                                       "add",
+                                                                       f.variableReference("value")),
+                                          f._return(f._this())
+                    );
+                }
+            };
+        }
+    };
+
+    private static MethodImplementation ALL_ADDER_IMPL = new MethodImplementation() {
+        @Override
+        public PieceOfCode createBody(final EntityContext context, final MethodModel method) {
+            return new PieceOfCode() {
+                @NotNull
+                @Override
+                public <E> E create(@NotNull CodeFactory<E> f) {
+                    Relation<?> relation = method.getData(ALL_ADDER);
+                    return CodeUtil.block(f,
+                                          CodeUtil.methodCallStatement(f,
+                                                                       f.fieldReference(f._this(),
+                                                                                        context.getField(relation).getName()),
+                                                                       "addAll",
+                                                                       f.variableReference("values")),
+                                          f._return(f._this())
+                    );
+                }
+            };
+        }
+    };
+
+    private static Map<DataHolderKey<Relation<?>>, MethodImplementation> IMPLS = ImmutableMap.<DataHolderKey<Relation<?>>, MethodImplementation>builder()
+            .put(GETTER, GETTER_IMPL)
+            .put(SETTER, SETTER_IMPL)
+            .put(ADDER, ADDER_IMPL)
+            .put(ALL_ADDER, ALL_ADDER_IMPL)
+            .build();
+
+    private static class EntityContext {
         private final Entity entity;
         private final ClassBean classBean;
         private final EntityRepresentationContext<ClassBean> context;
         private final TypeTransformer types;
         private final Map<Relation<?>, FieldModel> fields = Maps.newHashMap();
 
-        private Context(EntityRepresentationContext<ClassBean> context, Entity entity, ClassBean classBean) {
+        private EntityContext(EntityRepresentationContext<ClassBean> context, Entity entity, ClassBean classBean) {
             this.context = context;
             this.types = types(context);
             this.entity = entity;
             this.classBean = classBean;
         }
 
-        public FieldModel getField(Relation<?> relation) {
-            return fields.get(relation);
+        public FieldModel getField(@NotNull Relation<?> relation) {
+            FieldModel field = fields.get(relation);
+            if (field == null) {
+                throw new IllegalArgumentException("No field for relation " + relation.getName());
+            }
+            return field;
         }
     }
 
