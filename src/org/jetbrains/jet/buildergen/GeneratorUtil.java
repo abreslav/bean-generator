0cb14cb9d82f0ab49e2791e840b8c199f90ce742
Andrey Breslav <andrey.breslav@jetbrains.com>
2012-11-02 09:45:03 +0400

    createMethodBody() utility

--- src/org/jetbrains/jet/buildergen/GeneratorUtil.java
+++ src/org/jetbrains/jet/buildergen/GeneratorUtil.java
@@ -16,15 +16,21 @@
 
 package org.jetbrains.jet.buildergen;
 
+import com.google.common.collect.Lists;
 import com.intellij.openapi.util.text.StringUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.jet.buildergen.entities.Entity;
 import org.jetbrains.jet.buildergen.entities.Relation;
+import org.jetbrains.jet.buildergen.java.ClassPrinter;
 import org.jetbrains.jet.buildergen.java.JavaKeyWords;
 import org.jetbrains.jet.buildergen.java.code.BinaryOperation;
 import org.jetbrains.jet.buildergen.java.code.CodeFactory;
+import org.jetbrains.jet.buildergen.java.code.PieceOfCode;
+import org.jetbrains.jet.buildergen.java.declarations.beans.MethodBean;
 import org.jetbrains.jet.buildergen.java.types.TypeUtil;
 import org.jetbrains.jet.buildergen.runtime.LiteralReference;
+
+import java.util.List;
 
 import static org.jetbrains.jet.buildergen.java.code.CodeUtil.classReference;
 import static org.jetbrains.jet.buildergen.java.code.CodeUtil.methodCallWithTypeArgument;
@@ -53,5 +59,25 @@ public class GeneratorUtil {
                         f._null()
                 ),
                 f._return(f._null()));
+    }
+
+    public interface MethodBody {
+        <E> void body(@NotNull CodeFactory<E> f, @NotNull List<E> statements);
+    }
+
+    public static void createMethodBody(@NotNull MethodBean method, @NotNull final MethodBody body) {
+        method.put(
+                ClassPrinter.METHOD_BODY,
+                new PieceOfCode() {
+                    @Override
+                    public <E> E create(@NotNull CodeFactory<E> f) {
+                        List<E> statements = Lists.newArrayList();
+
+                        body.body(f, statements);
+
+                        return f.block(statements);
+                    }
+                }
+        );
     }
 }
